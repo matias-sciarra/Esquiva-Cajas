61cf05115311b509bd5cc74f2eed21b3ece5a264
matias-sciarra <Matiassciarra1@gmail.com>
2026-05-07 11:39:56 -0300

    Terminado Caja Collision Enter

---
+++ Assets/Scripts/CajaCollisionDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDetection : MonoBehaviour
+{
+    public CajaMovement cajaMovementScript;
+     
+    void Start()
+    {
+        cajaMovementScript = GetComponent<CajaMovement>();
+    }
+
+    void OnCollisionEnter(Collision colo)
+    {
+        if (colo.gameObject.CompareTag("Suelo"))
+        {
+            cajaMovementScript.MoveBoxToRandomPosition();
+        }
+    }
+}
--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
     public float VelCaja = 0.1f;
-    public
+    public float AumentoVelocidad = 0.1f;
+    float initialYValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialYValue = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        VelCaja += AumentoVelocidad;
         transform.Translate(0, -VelCaja * Time.deltaTime, 0);
-        if (transform.position.y <= -0.5)
-        {
-            transform.Translate(0,9,0);
-        }
+        
+    }
+    public void MoveBoxToRandomPosition()
+    {
+        //Para probar, solamete hacemos que suba
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
