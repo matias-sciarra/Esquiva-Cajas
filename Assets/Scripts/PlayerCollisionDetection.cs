faefcc7d814209c8f5c54ac3db1246fe70dfe2ab
matias-sciarra <Matiassciarra1@gmail.com>
2026-05-07 10:35:31 -0300

    PlayerCollisionDetection terminated

---
+++ Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+   void OnCollisionEnter(Collision colo)
+   {
+        if (colo.gameObject.CompareTag("Caja"))
+        {
+            Destroy(gameObject);
+        }
+   }
+}
